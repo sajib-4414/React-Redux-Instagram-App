56c705982e98ae3989c8e34f8a3efcb22db468a9
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2023-05-19 18:51:04 -0600

    added basic react components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {React, Component} from 'react';
+import  ReactDOM  from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class List extends Component{
+    render(){
+        return (
+        <ol> 
+            {this.props.tasks.map((task,index) => <li key={index}>{task}</li>)}
+        </ol>
+        )
+    }
+}
+class Title extends Component{
+    render(){
+        return (
+            <h1>{this.props.title}</h1>
+        )
+    }
+}
+class Main extends Component{
+    render(){
+        return(
+            <div>
+                <Title title={'todos'}/>
+                <List tasks={['Mow the lawn','walk the dog']}/>
+                <List tasks={['hose the driveway','Finish the laundry']}/>
+            </div>
+        )
+    }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Main/>);
